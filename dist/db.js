3273c596ce1f0311ec50afb412ef7a65bea6f2b3
dohye1 <tsc03018@gmail.com>
2021-02-11 17:23:42 +0900

    [#1] env : mysql2를 사용해서 db에 데이터 생성

--- dist/db.js
+++ dist/db.js
@@ -1,14 +1,30 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mysql_1 = __importDefault(require("mysql"));
-const connection = mysql_1.default.createConnection({
+const promise_1 = __importDefault(require("mysql2/promise"));
+const pool = promise_1.default.createPool({
     host: 'localhost',
-    port: '3306',
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: 'studev'
+    port: 3306,
+    user: 'root',
+    password: '5872',
+    database: 'studev',
+    connectionLimit: 50000,
+    waitForConnections: false,
 });
-connection.connect(() => console.log('DB is connected....'));
+const get = () => __awaiter(void 0, void 0, void 0, function* () {
+    const connection = yield pool.getConnection();
+    const query = "insert into user(id, user_id, password, nickname) values(1, 'dd', '123', 'dd')";
+    connection.query(query);
+});
+get();
--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,21 @@
-import mysql from 'mysql';
-const connection = mysql.createConnection({
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
   host: 'localhost',
-  port: '3306',
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: 'studev'
+  port: 3306,
+  user: 'root',
+  password: '5872',
+  database: 'studev',
+  connectionLimit: 50000,
+  waitForConnections: false,
 });
 
-connection.connect(() => console.log('DB is connected....'));
+const get = async () => {
+  const connection = await pool.getConnection();
+  const query =
+    "insert into user(id, user_id, password, nickname) values(1, 'dd', '123', 'dd')";
+
+  connection.query(query);
+};
+
+get();
