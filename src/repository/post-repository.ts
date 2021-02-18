030e9c2616d69e14fcbd767a9070073669df6a97
dohye1 <tsc03018@gmail.com>
2021-02-18 13:38:13 +0900

    feat : post를 생성하는 model 로직 생성

---
+++ src/repository/post-repository.ts
@@ -0,0 +1,27 @@
+import {
+  insertQueryExecuter,
+  selectQueryExecuter,
+} from '../utils/query-executor';
+import { getFormatDate } from '../utils/get-data';
+
+export type Post = {
+  title: string;
+  content: string;
+  topic_category: string;
+  on_off_category: string;
+  participant_count_limit: number;
+};
+
+export class PostRepo {
+  static async createPost(
+    id: number,
+    title: string,
+    content: string,
+    topic_category: string,
+    on_off_category: string,
+    participant_count_limit: number,
+  ) {
+    const createPostQuery = `INSERT INTO post(user_id, title, content, topic_category, on_off_category, participant_count_limit) VALUES ('${id}', '${title}', '${content}', '${topic_category}', '${on_off_category}', '${participant_count_limit}')`;
+    return await insertQueryExecuter(createPostQuery);
+  }
+}
