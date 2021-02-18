9c913f359da0a76b8f2d32c7af971bc481a75198
dohye1 <tsc03018@gmail.com>
2021-02-18 13:39:53 +0900

    feat : jwt을 verify하는 로직

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,16 @@
 import jwt from 'jsonwebtoken';
+import { UserRepo } from '../repository/user-repository';
 
 export const createJWT = (id: number): string => {
   return jwt.sign({ id }, 'token');
 };
 
-//export const verifyJWT = async (token: string): Promise<User | null> => {};
+export const verifyJWT = async (token: string) => {
+  const verifyResult: any = jwt.verify(token, 'token');
+  if (!verifyResult) {
+    return null;
+  }
+
+  const { id } = verifyResult;
+  return await UserRepo.findById(id);
+};
