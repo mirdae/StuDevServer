af8bc66affdb9850969298c9541e3a348333cb07
dohye1 <tsc03018@gmail.com>
2021-02-23 21:46:19 +0900

    feat : 스터디 신청하는 api route 설정

--- src/routes/post-routes.ts
+++ src/routes/post-routes.ts
@@ -3,11 +3,13 @@ import {
   createPost,
   getAllPosts,
   getPostDetail,
+  participateApply,
 } from '../service/post-service';
 import { decodeJWT } from '../middleware/decode-jwt';
 
 const router = Router();
 
+router.get('/:id/participate', decodeJWT, participateApply);
 router.get('/:id', getPostDetail);
 router.post('/', decodeJWT, createPost);
 router.get('/', getAllPosts);
